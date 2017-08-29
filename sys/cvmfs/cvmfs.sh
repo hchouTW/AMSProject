350b6fabde4d37894660ca506e054e1f6dfa7d0b
Hsin-Yi Chou <hchou@cern.ch>
2017-08-29 13:30:38 +0800

    update cvmfs

--- sys/cvmfs/cvmfs.sh
+++
@@ -1,4 +0,0 @@
-yum localinstall http://cvmrepo.web.cern.ch/cvmrepo/yum/cvmfs/EL/6/x86_64/cvmfs-release-2-4.el6.noarch.rpm -y
-yum install cvmfs cvmfs-init-scripts cvmfs-auto-setup -y
-echo CVMFS_HTTP_PROXY=http://ca-proxy.cern.ch:3128 >> /etc/cvmfs/default.local
-ls /cvmfs/ams.cern.ch/Offline
