09baa0afa4c0d99e19e78b1c2b38c748d1beb6fb
Hsin-Yi Chou <hchou@cern.ch>
2018-02-02 13:04:36 +0100

    update

--- prod/run.sh
+++ prod/run.sh
@@ -8,14 +8,14 @@ RunFile=${AMSCore}/prod/${Version}/YiProdNtuple
 #DataType=ISS
 #Stream=lst/flist.ncu.iss.B950.pass6
 DataType=MC
-Stream=lst/flist.cern.mc.pr.pl1.ecal.10004000.B1003
+#Stream=lst/flist.cern.mc.pr.pl1.ecal.10004000.B1103
 #Stream=lst/flist.cern.mc.pr.pl1.1800.B1042
-#Stream=lst/flist.cern.mc.pr.pl1.l1.054000.B1119
+Stream=lst/flist.cern.mc.pr.pl1.l1.054000.B1119
 #Stream=lst/flist.cern.mc.el.pl1.0_25200.B1119
 #Stream=lst/flist.cern.mc.el.pl1.2004000.B1119
 
 GroupId=6
-GroupSize=1
+GroupSize=30
 OutputDir=.
 
 if [[ -f ${RunFile} && -f ${Stream} ]]; then
