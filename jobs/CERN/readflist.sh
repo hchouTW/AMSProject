5ee71a0d4e1766b1a30dc0de0367ad36507eda86
Hsin-Yi Chou <hchou@cern.ch>
2018-02-16 17:31:29 +0000

    update jobs struct

---
+++ jobs/CERN/readflist.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Program:
+# 	read root file list. 
+# History:
+# 	2013/10/17 hchou First release
+# 	2014/06/07 hchou modify
+# 	2017/09/04 hchou modify
+shopt -s -o nounset
+
+if [ $# -lt 2 ] && [ $# -ne 3 ]; then
+    echo "illegal number of parameters."
+    exit
+fi
+
+head=
+if [ $# -eq 3 ]; then
+    head=$3
+fi
+
+flist=$1
+dataDir=$2
+
+ls $dataDir | grep root > ./$flist
+sed -i 's'',^'",${head}${dataDir}/"',g' ./$flist
+
+echo "---- READ FILE LIST ----"
+echo "FLIST : " $flist
+echo "PATH  : " $dataDir
---
+++ jobs/NCU/xrdcp_cern2ncu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+input=$1
+output=$2
+keyword=$3
+
+if [[ ${input} == "" ]]; then exit; fi
+if [[ ${output} == "" ]]; then exit; fi
+
+dpmurl=root://${DPNS_HOST}/${output}
+dpmhost=${DPM_HOME}/${output}
+dpns-mkdir -p ${dpmhost}
+for file in `xrdfs ${EOS_AMS_HOST} ls ${input}`
+do
+    if [[ `echo ${file} | grep ${keyword}` == "" ]]; then continue; fi
+    filename=${file##*/}
+    filecheck=$(dpns-ls ${dpmhost}/${filename} 2>&1)
+    if [[ "${filecheck}" == *"No such file or directory"* ]] || [[ "${filecheck}" == *"invalid path"* ]]; then
+        srcfile=root://eosams.cern.ch/${file}
+        echo -e "==== xrdcp ${srcfile} ${dpmurl}/${filename}"
+        xrdcp ${srcfile} ${dpmurl}/${filename}
+    else
+        echo -e "==== ${filename} is exist."
+    fi
+done
