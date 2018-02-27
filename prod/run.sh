f1c802c932961c05d914affa6c3c92e08fefaa7e
Hsin-Yi Chou <hchou@cern.ch>
2018-02-27 05:59:12 +0100

    update

--- prod/run.sh
+++ prod/run.sh
@@ -5,15 +5,15 @@ ClassDef=${AMSCore}/prod/${Version}/lib
 LD_LIBRARY_PATH=${ClassDef}:${LD_LIBRARY_PATH}
 RunFile=${AMSCore}/prod/${Version}/YiProdNtuple
 
-DataType=ISS
+#DataType=ISS
 #Stream=lst/flist.asgc.iss.B950.pass6
-Stream=lst/flist.ncu.iss.B950.pass6
+#Stream=lst/flist.ncu.iss.B950.pass6
 #Stream=lst/flist.cern.iss.B950.pass6
-#DataType=MC
+DataType=MC
 #Stream=lst/flist.ncu.mc.pr.pl1.l1.054000.B1119
 #Stream=lst/flist.cern.mc.pr.pl1.ecal.10004000.B1103
 #Stream=lst/flist.cern.mc.pr.pl1.1800.B1042
-#Stream=lst/flist.cern.mc.pr.pl1.l1.054000.B1119
+Stream=lst/flist.cern.mc.pr.pl1.l1.054000.B1119
 #Stream=lst/flist.cern.mc.el.pl1.0_25200.B1119
 #Stream=lst/flist.cern.mc.el.pl1.2004000.B1119
 
