fa48a67f9b7f52c2f466edfb616cfd708824d013
Hsin-Yi Chou <hchou@cern.ch>
2018-03-04 16:58:13 +0000

    update

--- libs/external/google/gflags/bin/gflags.sh
+++ libs/external/google/gflags/bin/gflags.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=${AMSProjLibs}/external/google/gflags/lib64:${LD_LIBRARY_PATH}
-export CPLUS_INCLUDE_PATH=${AMSProjLibs}/external/google/gflags/inlcude:${CPLUS_INCLUDE_PATH}
+export CPLUS_INCLUDE_PATH=${AMSProjLibs}/external/google/gflags/include:${CPLUS_INCLUDE_PATH}
