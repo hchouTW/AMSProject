f5ad06268583bae8a054adf2dd876b1ed2d19b19
Hsin-Yi Chou <hchou@cern.ch>
2018-03-08 18:30:25 +0000

    update

--- libs/TRACKLibs/MagEnv.C
+++ libs/TRACKLibs/MagEnv.C
@@ -290,9 +290,6 @@ Bool_t MagMgnt::Load() {
 #ifdef __HAS_AMS_OFFICE_LIBS__
     is_load_ = MagGeoBoxAms::Load();
 #endif // __HAS_AMS_OFFICE_LIBS__
-#ifdef __HAS_TESTPROP__
-    is_load_ = false;
-#endif // __HAS_TESTPROP__
 #ifdef __HAS_TESTFIT__
     if (!geo_box_reader_.exist()) {
         std::string file_path = "/data3/hchou/AMSData/MagDB/MagTest.bin";
@@ -311,11 +308,6 @@ MagFld MagMgnt::Get(const SVecD<3>& coo) {
 #ifdef __HAS_AMS_OFFICE_LIBS__
     return MagGeoBoxAms::Get(coo);
 #endif // __HAS_AMS_OFFICE_LIBS__
-
-#ifdef __HAS_TESTPROP__
-    return MagFld();
-#endif // __HAS_TESTPROP__
-
 #ifdef __HAS_TESTFIT__
     return geo_box_reader_.get(coo);
 #endif // __HAS_TESTFIT__
