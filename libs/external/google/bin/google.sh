bd56f645e5355c019051a5ebbe411bf9f5ef530b
Hsin-Yi Chou <hchou@cern.ch>
2018-03-10 20:40:02 +0000

    update

--- libs/external/google/bin/google.sh
+++ libs/external/google/bin/google.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export LIBRARY_PATH=${AMSProjLibs}/external/google/lib:${LIBRARY_PATH}
+export LD_LIBRARY_PATH=${AMSProjLibs}/external/google/lib:${LD_LIBRARY_PATH}
 export LIBRARY_PATH=${AMSProjLibs}/external/google/lib64:${LIBRARY_PATH}
 export LD_LIBRARY_PATH=${AMSProjLibs}/external/google/lib64:${LD_LIBRARY_PATH}
 export CPLUS_INCLUDE_PATH=${AMSProjLibs}/external/google/include:${CPLUS_INCLUDE_PATH}
