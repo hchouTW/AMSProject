4e3752ac60d8daba6f0558af74750e12d64f0f94
Hsin-Yi Chou <hchou@cern.ch>
2018-04-02 16:32:04 +0000

    update

---
+++ libs/TRACKSys/include/InterfaceAms.C
@@ -0,0 +1,13 @@
+#ifndef __TRACKLibs_InterfaceAms_C__
+#define __TRACKLibs_InterfaceAms_C__
+
+
+namespace TrackSys {
+namespace InterfaceAms {
+
+
+} // namespace InterfaceAms
+} // namespace TrackSys
+
+
+#endif // __TRACKLibs_InterfaceAms_C__
---
+++ libs/TRACKSys/include/InterfaceAms.h
@@ -0,0 +1,13 @@
+#ifndef __TRACKLibs_InterfaceAms_H__
+#define __TRACKLibs_InterfaceAms_H__
+
+
+namespace TrackSys {
+namespace InterfaceAms {
+
+
+} // namespace InterfaceAms
+} // namespace TrackSys
+
+
+#endif // __TRACKLibs_InterfaceAms_H__
