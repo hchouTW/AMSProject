1a516c59898f32499f366131a645238c3e1cc987
Hsin-Yi Chou <hchou@cern.ch>
2018-04-07 16:32:46 +0000

    TODO InterfaceAms for AMS-Exp

--- libs/TRACKSys/include/InterfaceAms.h
+++ libs/TRACKSys/include/InterfaceAms.h
@@ -6,6 +6,14 @@
 namespace TrackSys {
 namespace InterfaceAms {
 
+// TODO :
+//static HitStTRK GetHitStTRK(const TrRecHitR& hit, Bool_t hasQ = false);
+//static HitStTOF GetHitStTOF(const TofClusterHR& cls, Bool_t hasQ = false, Bool_t hasT = false);
+//
+// TODO :
+//static TrFitPar GetTrackPar(const std::vector<HitStTRK>& trk, const std::vector<HitStTOF>& tof);
+//static TrFitPar GetInnerTrackPar(const AMSEventR& event, Bool_t hasTOF = false);
+//static TrFitPar GetFullSpanTrackPar(const AMSEventR& event, Bool_t hasTOF = false);
 
 } // namespace InterfaceAms
 } // namespace TrackSys
