f983632b40ac3580ae301eddc061f5f01f886e51
Hsin-Yi Chou <hchou@cern.ch>
2018-10-03 17:06:33 +0000

    update

--- subj/trsys/cp.sh
+++ subj/trsys/cp.sh
@@ -4,7 +4,7 @@ for id in `seq 0 33`
 #for id in `seq 0 39`
 do
     ii=`printf "%05d" $id`
-    sh /ams_home/hchou/AMSProject/jobs/NCU/xrdcp_cern2ncu.sh /eos/ams/user/h/hchou/AMSData/prod/18Sep25/PR_054000.B1200 /ams02/user/hchou/PR_054000.B1200_18Sep25 YiNtuple_MC.${ii} &
+    sh /ams_home/hchou/AMSProject/jobs/NCU/xrdcp_cern2ncu.sh /eos/ams/user/h/hchou/AMSData/prod/18Oct03/PR_054000.B1200 /ams02/user/hchou/PR_054000.B1200_18Oct03 YiNtuple_MC.${ii} &
     #sh /ams_home/hchou/AMSProject/jobs/NCU/xrdcp_cern2ncu.sh /eos/ams/user/h/hchou/AMSData/prod/18Sep21/HE4_24000.B1200 /ams02/user/hchou/HE4_24000.B1200_18Sep21 YiNtuple_MC.${ii} &
 done
 
